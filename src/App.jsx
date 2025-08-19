ea10b79803c1435ba4cbbe89b9c2ad03a53ea189
BjfpgZOC <ggovinda@student.ethz.ch>
2025-08-19 21:22:12 +0200

    Analytics bug fix.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Orb from "./Orb.jsx";
 import GlassSurface from "./GlassSurface.jsx";
 import "./Orb.css";
